bc82f233bb1d4033a8db7f60c9b51b88c784a133
Ayush Raj <oneayush11@gmail.com>
2026-08-22 03:31:25 +0530

    Update axios baseURL to use environment variable
    Fix production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // Vite dev server proxies /api -> http://localhost:5001 (see vite.config.js)
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "/api",
 });
 
 api.interceptors.request.use((config) => {
